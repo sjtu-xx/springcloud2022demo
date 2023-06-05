5b527ea1cc250c12536cd6c7ccb1f5389cd050bd
xuexuan <1099313374@qq.com>
2023-06-05 23:28:05 +0800

    提交简单的微服务

--- cloud-provider-user-8001/src/main/java/com/xx/api/UserController.java
+++ cloud-provider-user-8001/src/main/java/com/xx/api/UserController.java
@@ -1,10 +1,9 @@
 package com.xx.api;
 
+import com.xx.core.CommonResult;
 import com.xx.domain.entity.User;
 import com.xx.service.IUserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +27,15 @@ public class UserController {
     @GetMapping("/listAll")
     public List<User> listAll() {
         return userService.list();
+    }
+
+    @GetMapping("/get/{id}")
+    public CommonResult<User> getById(@PathVariable("id") Integer id) {
+        return CommonResult.successReturn(userService.getById(id));
+    }
+    
+    @PostMapping("/create")
+    public CommonResult<Boolean> create(@RequestBody User user) {
+        return CommonResult.successReturn(userService.save(user));
     }
 }
