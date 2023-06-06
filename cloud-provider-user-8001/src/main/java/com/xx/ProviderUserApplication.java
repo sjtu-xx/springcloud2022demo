4f0c1b8539c4688a0749692beb8b9987b7d9b38c
xuexuan <1099313374@qq.com>
2023-06-06 23:42:56 +0800

    提交补充代码

--- cloud-provider-user-8001/src/main/java/com/xx/ProviderUserApplication.java
+++ cloud-provider-user-8001/src/main/java/com/xx/ProviderUserApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProviderPaymentApplication {
+public class ProviderUserApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ProviderPaymentApplication.class, args);
+        SpringApplication.run(ProviderUserApplication.class, args);
     }
 }
