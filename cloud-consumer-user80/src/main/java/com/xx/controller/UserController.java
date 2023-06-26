3eb361794c9c3ca5c8c199f6af16bc3b9cb24f0b
xuexuan <1099313374@qq.com>
2023-06-26 23:01:40 +0800

    增加消费者

---
+++ cloud-consumer-user80/src/main/java/com/xx/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.xx.controller;
+
+import com.xx.core.CommonResult;
+import com.xx.domain.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class UserController {
+    public static final String USER_URL = "http://localhost:8001";
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @PostMapping("/consumer/user/create")
+    public CommonResult<User> create(@RequestBody User user) {
+        return restTemplate.postForObject(USER_URL + "/user/create", user, CommonResult.class);
+    }
+
+    @GetMapping("/consumer/user/get/{id}")
+    public CommonResult<User> getUser(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(USER_URL + "/user/get/" + id, CommonResult.class);
+    }
+}
--- cloud-consumer-user80/src/main/java/com/xx/domain/entity/User.java
+++ cloud-consumer-user80/src/main/java/com/xx/domain/entity/User.java
@@ -1,23 +1,19 @@
 package com.xx.domain.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
-
 import java.io.Serializable;
 
 /**
  * <p>
- * 
+ *
  * </p>
  *
  * @author xuexuan
  * @since 2023-06-02
  */
-@TableName("public.user")
 public class User implements Serializable {
 
-    @TableId(value = "id", type = IdType.AUTO)
+    private static final long serialVersionUID = 1L;
+
     private Integer id;
 
     private String name;
