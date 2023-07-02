54883269bf9024d121b72caa1914421aeed10c7e
xuexuan <1099313374@qq.com>
2023-07-02 22:34:21 +0800

    [gateway] gateway网关服务

---
+++ cloud-gateway-8429/src/main/java/com/xx/CloudGateway.java
@@ -0,0 +1,11 @@
+package com.xx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudGateway {
+    public static void main(String[] args) {
+        SpringApplication.run(CloudGateway.class, args);
+    }
+}
