21ce04446c00f38aac8e33362a2de0c39981be01
xuexuan <1099313374@qq.com>
2023-07-02 17:32:22 +0800

    [hystrix] hystrix已经被remove，没有意义

--- cloud-provider-hystrix-user8007/src/main/java/com/xx/UserProviderHystrix8007.java
+++ cloud-provider-hystrix-user8007/src/main/java/com/xx/UserProviderHystrix8007.java
@@ -2,6 +2,7 @@ package com.xx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 public class UserProviderHystrix8007 {
--- cloud-provider-hystrix-user8007/src/main/java/com/xx/service/UserService.java
+++ cloud-provider-hystrix-user8007/src/main/java/com/xx/service/UserService.java
@@ -1,5 +1,7 @@
 package com.xx.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -10,6 +12,10 @@ public class UserService {
         return "线程" + Thread.currentThread().getName() + " id : " + id;
     }
 
+    @HystrixCommand(fallbackMethod = "userTimeoutHandler",
+    commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000")
+    })
     public String userTimeout(Integer id) {
         try {
             TimeUnit.SECONDS.sleep(5);
@@ -17,5 +23,9 @@ public class UserService {
             throw new RuntimeException(e);
         }
         return "线程" + Thread.currentThread().getName() + " id : " + id + " 耗时5秒";
+    }
+
+    public String userTimeoutHandler(Integer id) {
+        return "线程" + Thread.currentThread().getName() + " id : " + id + " fallback";
     }
 }
