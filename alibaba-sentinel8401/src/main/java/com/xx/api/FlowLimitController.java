feb7db058a72e5e961122144b14d472b3a90a3c2
xuexuan <1099313374@qq.com>
2023-07-22 22:48:04 +0800

    [sentinel] 工程

---
+++ alibaba-sentinel8401/src/main/java/com/xx/api/FlowLimitController.java
@@ -0,0 +1,31 @@
+package com.xx.api;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FlowLimitController {
+    @GetMapping("/testA")
+    public String testA() {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+        return "------testB";
+    }
+
+    @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey", blockHandler = "deal_testHotKey") //不配置blockhandler会导致前台异常
+    public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
+                             @RequestParam(value = "p2", required = false) String p2) {
+        return "------testHotKey";
+    }
+
+    public String deal_testHotKey(String p1, String p2, BlockException e) {
+        return "------deal_testHotKey,o(╥，╥)o"; //sentinel熔断降离返回的插件
+    }
+}
